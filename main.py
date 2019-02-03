8611074d966a90d046a0393d5c966a36ff46412e
SCPO <47129204+SCPO@users.noreply.github.com>
2019-02-02 21:20:53 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -21,78 +21,199 @@ trainData = trainData.reshape([3500,784]) #train data matrix
 testData = testData.reshape([145,784]) #test data matrix
 validData = validData.reshape([100,784]) #validation data matrix
 alpha = 0.005
-epochs = 5000
+epochs = 5000 #5000
 
 
 
-#%% Question 3, Tuning Learning Rate
+#==============================================================================
+# #%% Question 3, Tuning Learning Rate
+# 
+# reg = 0;
+# alphaParams = [0.005, 0.001, 0.0001]
+# perfRecordAll = {}
+# 
+# for i in range(0, len(alphaParams)): 
+#     #reset the model
+#     W = np.random.normal(0, 0.1, size = [784,1])
+#     b = 0 #np.zeros((3500,1)) #bias matrix
+#     
+#     alpha = alphaParams[i]
+#     W,b, perfRecord = starter.grad_descent(W, b, trainData, trainTarget, alpha, epochs, reg, error_tol, \
+#                                            validData, validTarget, testData, testTarget )
+#     
+#     perfRecordAll["{0}".format(alphaParams[i])] = perfRecord #save the data
+#     
+#     
+# #%% Make the plots of errors and accuracies 
+# #errors
+# plt.figure(figsize=(15,15))
+# plt.subplot(2, 2, 1)
+# plt.ylabel('Error')
+# plt.xlabel('Iterations')
+# plt.title('TrainingSet Error of Different Alpha vs. Iterations of Gradient Descent')
+# plt.plot(perfRecordAll["0.005"].errorTrain, 'r')
+# plt.plot(perfRecordAll["0.001"].errorTrain, 'b') 
+# plt.plot(perfRecordAll["0.0001"].errorTrain, 'g') 
+# plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorTrain[-1] \
+#             ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorTrain[-1]\
+#             , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorTrain[-1]])
+# 
+# plt.subplot(2, 2, 2)
+# plt.ylabel('Error')
+# plt.xlabel('Iterations')
+# plt.title('ValidSet Error of Different Alpha vs. Iterations of Gradient Descent')
+# plt.plot(perfRecordAll["0.005"].errorValid, 'r')
+# plt.plot(perfRecordAll["0.001"].errorValid, 'b') 
+# plt.plot(perfRecordAll["0.0001"].errorValid, 'g') 
+# plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorValid[-1] \
+#             ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorValid[-1]\
+#             , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorValid[-1]])
+# 
+# plt.subplot(2, 2, 3)
+# plt.ylabel('Error')
+# plt.xlabel('Iterations')
+# plt.title('TestSet Error of Different Alpha vs. Iterations of Gradient Descent')
+# plt.plot(perfRecordAll["0.005"].errorTest, 'r')
+# plt.plot(perfRecordAll["0.001"].errorTest, 'b') 
+# plt.plot(perfRecordAll["0.0001"].errorTest, 'g') 
+# plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorTest[-1] \
+#             ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorTest[-1]\
+#             , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorTest[-1]])
+# 
+# 
+# #accuracies
+# plt.figure(figsize=(15,15))
+# plt.subplot(2, 2, 1)
+# plt.ylabel('Accuracy')
+# plt.xlabel('Iterations')
+# plt.title('TrainingSet Accuracy of Different Alpha vs. Iterations')
+# plt.plot(perfRecordAll["0.005"].trainSetAcc, 'r')
+# plt.plot(perfRecordAll["0.001"].trainSetAcc, 'b') 
+# plt.plot(perfRecordAll["0.0001"].trainSetAcc, 'g') 
+# plt.legend(['alpha 0.005, fin. acc = %f' % perfRecordAll["0.005"].trainSetAcc[-1] \
+#             ,'alpha 0.001 fin. acc = %f' % perfRecordAll["0.001"].trainSetAcc[-1]\
+#             , 'alpha 0.0001 fin. acc = %f' % perfRecordAll["0.0001"].trainSetAcc[-1]], loc = 4)
+# 
+# plt.subplot(2, 2, 2)
+# plt.ylabel('Accuracy')
+# plt.xlabel('Iterations')
+# plt.title('ValidSet Accuracy of Different Alpha vs. Iterations')
+# plt.plot(perfRecordAll["0.005"].validSetAcc, 'r')
+# plt.plot(perfRecordAll["0.001"].validSetAcc, 'b') 
+# plt.plot(perfRecordAll["0.0001"].validSetAcc, 'g') 
+# plt.legend(['alpha 0.005, fin. acc = %f' % perfRecordAll["0.005"].validSetAcc[-1] \
+#             ,'alpha 0.001 fin. acc = %f' % perfRecordAll["0.001"].validSetAcc[-1]\
+#             , 'alpha 0.0001 fin. acc = %f' % perfRecordAll["0.0001"].validSetAcc[-1]], loc = 4)
+# 
+# plt.subplot(2, 2, 3)
+# plt.ylabel('Accuracy')
+# plt.xlabel('Iterations')
+# plt.title('TestSet Accuracy of Different Alpha vs. Iterations o')
+# plt.plot(perfRecordAll["0.005"].testSetAcc, 'r')
+# plt.plot(perfRecordAll["0.001"].testSetAcc, 'b') 
+# plt.plot(perfRecordAll["0.0001"].testSetAcc, 'g') 
+# plt.legend(['alpha 0.005, fin. acc = %f' % perfRecordAll["0.005"].testSetAcc[-1] \
+#             ,'alpha 0.001 fin. acc = %f' % perfRecordAll["0.001"].testSetAcc[-1]\
+#             , 'alpha 0.0001 fin. acc = %f' % perfRecordAll["0.0001"].testSetAcc[-1]], loc = 4)
+# 
+# 
+#==============================================================================
 
-reg = 0;
-alphaParams = [0.005, 0.001, 0.0001]
+
+#%% Question 4, 
+
+alpha = 0.005;
+regParams = [0.001, 0.1, 0.5]
 perfRecordAll = {}
 
-for i in range(0, len(alphaParams)): 
+for i in range(0, len(regParams)): 
     #reset the model
     W = np.random.normal(0, 0.1, size = [784,1])
     b = 0#np.zeros((3500,1)) #bias matrix
     
-    alpha = alphaParams[i]
+    reg = regParams[i]
     W,b, perfRecord = starter.grad_descent(W, b, trainData, trainTarget, alpha, epochs, reg, error_tol, \
                                            validData, validTarget, testData, testTarget )
     
-    perfRecordAll["{0}".format(alphaParams[i])] = perfRecord #save the data
+    perfRecordAll["{0}".format(regParams[i])] = perfRecord #save the data
     
     
-    #Make the plots of errors and accuracies 
-    #errors
-    plt.figure(figsize=(15,15))
-    plt.subplot(2, 2, 1)
-    plt.ylabel('Error')
-    plt.xlabel('Iterations')
-    plt.title('TrainingSet Error of Different Alpha vs. Interations of Gradient Descent')
-    plt.plot(perfRecordAll["0.005"].errorTrain, 'r')
-    plt.plot(perfRecordAll["0.001"].errorTrain, 'b') 
-    plt.plot(perfRecordAll["0.0001"].errorTrain, 'g') 
-    plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorTrain[-1] \
-                ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorTrain[-1]\
-                , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorTrain[-1]])
-    
-    plt.subplot(2, 2, 2)
-    plt.ylabel('Error')
-    plt.xlabel('Iterations')
-    plt.title('ValidSet Error of Different Alpha vs. Interations of Gradient Descent')
-    plt.plot(perfRecordAll["0.005"].errorValid, 'r')
-    plt.plot(perfRecordAll["0.001"].errorValid, 'b') 
-    plt.plot(perfRecordAll["0.0001"].errorValid, 'g') 
-    plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorValid[-1] \
-                ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorValid[-1]\
-                , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorValid[-1]])
+#%% Make the plots of errors and accuracies 
+#errors
+plt.figure(figsize=(15,15))
+plt.subplot(2, 2, 1)
+plt.ylabel('Error')
+plt.xlabel('Iterations')
+plt.title('TrainingSet Error of Different Reg vs. Iterations of Gradient Descent')
+plt.plot(perfRecordAll["0.001"].errorTrain, 'r')
+plt.plot(perfRecordAll["0.1"].errorTrain, 'b') 
+plt.plot(perfRecordAll["0.5"].errorTrain, 'g') 
+plt.legend(['reg 0.001, fin. error = %f' % perfRecordAll["0.001"].errorTrain[-1] \
+            ,'reg 0.1 fin. error = %f' % perfRecordAll["0.1"].errorTrain[-1]\
+            , 'reg 0.5 fin. error = %f' % perfRecordAll["0.5"].errorTrain[-1]])
 
-    plt.subplot(2, 2, 3)
-    plt.ylabel('Error')
-    plt.xlabel('Iterations')
-    plt.title('TestSet Error of Different Alpha vs. Interations of Gradient Descent')
-    plt.plot(perfRecordAll["0.005"].errorTest, 'r')
-    plt.plot(perfRecordAll["0.001"].errorTest, 'b') 
-    plt.plot(perfRecordAll["0.0001"].errorTest, 'g') 
-    plt.legend(['alpha 0.005, fin. error = %f' % perfRecordAll["0.005"].errorTest[-1] \
-                ,'alpha 0.001 fin. error = %f' % perfRecordAll["0.001"].errorTest[-1]\
-                , 'alpha 0.0001 fin. error = %f' % perfRecordAll["0.0001"].errorTest[-1]])
-    
-    
-    
-    #print final accuracies: 
-    string = "0.005"
-    perfRecordAll[string].errorTrain[-1] #print final errors
-    perfRecordAll[string].errorValid[-1] #print final errors
-    perfRecordAll[string].errorTest[-1] #print final errors
-    perfRecordAll[string].trainSetAcc[-1]
-    perfRecordAll[string].validSetAcc[-1]
-    perfRecordAll[string].testSetAcc[-1]
+plt.subplot(2, 2, 2)
+plt.ylabel('Error')
+plt.xlabel('Iterations')
+plt.title('ValidSet Error of Different Reg vs. Iterations of Gradient Descent')
+plt.plot(perfRecordAll["0.001"].errorValid, 'r')
+plt.plot(perfRecordAll["0.1"].errorValid, 'b') 
+plt.plot(perfRecordAll["0.5"].errorValid, 'g') 
+plt.legend(['reg 0.001, fin. error = %f' % perfRecordAll["0.001"].errorValid[-1] \
+            ,'reg 0.1 fin. error = %f' % perfRecordAll["0.1"].errorValid[-1]\
+            , 'reg 0.5 fin. error = %f' % perfRecordAll["0.5"].errorValid[-1]])
+
+plt.subplot(2, 2, 3)
+plt.ylabel('Error')
+plt.xlabel('Iterations')
+plt.title('TestSet Error of Different reg vs. Iterations of Gradient Descent')
+plt.plot(perfRecordAll["0.001"].errorTest, 'r')
+plt.plot(perfRecordAll["0.1"].errorTest, 'b') 
+plt.plot(perfRecordAll["0.5"].errorTest, 'g') 
+plt.legend(['reg 0.001, fin. error = %f' % perfRecordAll["0.001"].errorTest[-1] \
+            ,'reg 0.1 fin. error = %f' % perfRecordAll["0.1"].errorTest[-1]\
+            , 'reg 0.5 fin. error = %f' % perfRecordAll["0.5"].errorTest[-1]])
 
 
 
 
+plt.figure(figsize = (20,20))
+plt.plot(perfRecordAll["0.001"].errorTest);plt.plot(perfRecordAll["0.001"].errorTrain);plt.plot(perfRecordAll["0.001"].errorValid);
+
+#accuracies
+plt.figure(figsize=(15,15))
+plt.subplot(2, 2, 1)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('TrainingSet Accuracy of Different reg vs. Iterations')
+plt.plot(perfRecordAll["0.001"].trainSetAcc, 'r')
+plt.plot(perfRecordAll["0.1"].trainSetAcc, 'b') 
+plt.plot(perfRecordAll["0.5"].trainSetAcc, 'g') 
+plt.legend(['reg 0.001, fin. acc = %f' % perfRecordAll["0.001"].trainSetAcc[-1] \
+            ,'reg 0.1 fin. acc = %f' % perfRecordAll["0.1"].trainSetAcc[-1]\
+            , 'reg 0.5 fin. acc = %f' % perfRecordAll["0.5"].trainSetAcc[-1]], loc = 4)
+
+plt.subplot(2, 2, 2)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('ValidSet Accuracy of Different reg vs. Iterations')
+plt.plot(perfRecordAll["0.001"].validSetAcc, 'r')
+plt.plot(perfRecordAll["0.1"].validSetAcc, 'b') 
+plt.plot(perfRecordAll["0.5"].validSetAcc, 'g') 
+plt.legend(['reg 0.001, fin. acc = %f' % perfRecordAll["0.001"].validSetAcc[-1] \
+            ,'reg 0.1 fin. acc = %f' % perfRecordAll["0.1"].validSetAcc[-1]\
+            , 'reg 0.5 fin. acc = %f' % perfRecordAll["0.5"].validSetAcc[-1]], loc = 4)
+
+plt.subplot(2, 2, 3)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('TestSet Accuracy of Different reg vs. Iterations o')
+plt.plot(perfRecordAll["0.001"].testSetAcc, 'r')
+plt.plot(perfRecordAll["0.1"].testSetAcc, 'b') 
+plt.plot(perfRecordAll["0.5"].testSetAcc, 'g') 
+plt.legend(['reg 0.001, fin. acc = %f' % perfRecordAll["0.001"].testSetAcc[-1] \
+            ,'reg 0.1 fin. acc = %f' % perfRecordAll["0.001"].testSetAcc[-1]\
+            , 'reg 0.5 fin. acc = %f' % perfRecordAll["0.5"].testSetAcc[-1]], loc = 4)
 
 
 
--- starter.py
+++ starter.py
@@ -6,12 +6,12 @@ import matplotlib.pyplot as plt
 class Performance:
     def __init__(self, iterations): #needed to initialize arrays
         self.iterations = iterations
-        self.errorTrain = np.empty(iterations)
-        self.errorValid = np.empty(iterations)
-        self.errorTest = np.empty(iterations)
-        self.trainSetAcc = np.empty(iterations)
-        self.validSetAcc = np.empty(iterations)
-        self.testSetAcc = np.empty(iterations)
+        self.errorTrain = np.zeros(iterations)*np.nan
+        self.errorValid = np.zeros(iterations)*np.nan
+        self.errorTest = np.zeros(iterations)*np.nan
+        self.trainSetAcc = np.zeros(iterations)*np.nan
+        self.validSetAcc = np.zeros(iterations)*np.nan
+        self.testSetAcc = np.zeros(iterations)*np.nan
         
         
 def loadData():
@@ -68,6 +68,7 @@ def grad_descent(W, b, trainData, trainTarget, alpha, iterations, reg, error_tol
     
     errorDifference = 0 #used in stopping condition
     oldError = np.inf
+    
     for i in range(0, iterations):
         error = MSE(W, b, trainData, trainTarget, reg)
         grad_wrtb, grad_wrtW = gradMSE(W, b, trainData, trainTarget, reg)
@@ -90,11 +91,19 @@ def grad_descent(W, b, trainData, trainTarget, alpha, iterations, reg, error_tol
         errorDifference = oldError - error 
         oldError = error
         print('error diff is %f' % errorDifference)
+        
         if errorDifference < error_tol:
-            print("error_tol stop condition met")
+            
+            perfRecord.testSetAcc.resize(i)
+            perfRecord.validSetAcc.resize(i)
+            perfRecord.trainSetAcc.resize(i)
+            perfRecord.errorTest.resize(i)
+            perfRecord.errorValid.resize(i)
+            perfRecord.errorTrain.resize(i)
+
+            print("error_tol stop condition met") 
             break
-        
-        
+
     return W, b, perfRecord
     
 def buildGraph(beta1=None, beta2=None, epsilon=None, lossType=None, learning_rate=None):
