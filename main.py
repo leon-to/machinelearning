0e7c454b3044d2b60bdf5575b8821843a1c77a9b
SCPO <47129204+SCPO@users.noreply.github.com>
2019-02-05 22:45:52 -0500

    Add files via upload
    Finished code for lab

--- main.py
+++ main.py
@@ -26,9 +26,151 @@ alpha = 0.005
 epochs = 5000 #5000
 
 
+#%% Q4c, MSE
 
+epochs = 700
+batch_size = 500
+epsParams = [1e-09, 1e-04]
+B1=0.9; B2 = 0.999;
 
+perfRecordAll = {}
+    
+for k in range(0, len(epsParams)):
+    eps = epsParams[k]
+    W, b, y_pred, x, y, loss, training_op, reg = starter.buildGraph(lossType = 'MSE', B1 = B1, B2 = B2, eps = eps)
+    #Question 3, batch gradient descent
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:   
+        sess.run(init) #make a new graph. 
+        
+        n = len(trainTarget)
+        n_batches = int(n/batch_size)
+        perfRecord = starter.Performance(epochs)
+        
+        for i in range(epochs):
+            indices = np.random.permutation(n)
+            featVectors = trainData[indices]
+            classes = trainTarget[indices]    
+            
+            for j in range(0, n, batch_size):
+                            
+                #print(loss.eval())
+                sess.run(training_op, feed_dict = {x: featVectors, y: classes})
+                error = sess.run(loss, feed_dict = {x: featVectors, y: classes})
+                Weights = sess.run(W, feed_dict = {x: featVectors, y: classes})
+                Bias = sess.run(b, feed_dict = {x: featVectors, y: classes})                
+                _, accuracy, _ = starter.classify(Weights, Bias, trainData, trainTarget)
+                
+        
+            print('epoch #%i' % i)
+            #In the process of implementing saving the performance
+            #Save the per iteration errors: Error, Training Set performance, 
+            #Validation Set Performance, Test Set performance 
+            perfRecord.errorTrain[i] = sess.run(loss, feed_dict = {x: trainData, y: trainTarget})
+            perfRecord.errorValid[i] = sess.run(loss, feed_dict = {x: validData, y: validTarget})
+            perfRecord.errorTest[i] = sess.run(loss, feed_dict = {x: testData, y: testTarget})
+            _, perfRecord.trainSetAcc[i] ,_ = starter.classify(Weights, Bias, trainData, trainTarget)
+            _, perfRecord.validSetAcc[i] ,_ = starter.classify(Weights, Bias, validData, validTarget)
+            _, perfRecord.testSetAcc[i] ,_ = starter.classify(Weights, Bias, testData, testTarget)
+            
+        perfRecordAll["{0}".format(epsParams[k])] = perfRecord #save the data     
 
+#%% Plots
+plt.figure(figsize=(15,7))
+plt.subplot(1, 2, 1)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('TrainingSet Accuracy of Different Epsilon vs. Iterations')
+plt.plot(perfRecordAll["1e-09"].trainSetAcc, 'r')
+plt.plot(perfRecordAll["0.0001"].trainSetAcc, 'b') 
+plt.legend(['Epsilon 1e-09, fin. acc = %f' % perfRecordAll["1e-09"].trainSetAcc[-1] \
+            ,'Epsilon 0.0001 fin. acc = %f' % perfRecordAll["0.0001"].trainSetAcc[-1]], loc = 4)
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('ValidSet Accuracy of Different Epsilon vs. Iterations')
+plt.plot(perfRecordAll["1e-09"].validSetAcc, 'r')
+plt.plot(perfRecordAll["0.0001"].validSetAcc, 'b') 
+plt.legend(['Epsilon 1e-09, fin. acc = %f' % perfRecordAll["1e-09"].validSetAcc[-1] \
+            ,'Epsilon 0.0001 fin. acc = %f' % perfRecordAll["0.0001"].validSetAcc[-1]], loc = 4)
+
+plt.savefig("Accuracy plot P3Q4c.png")
+                      
+
+#%% Q4b, MSE
+
+epochs = 700
+batch_size = 500
+B2params = [0.99, 0.9999] 
+B1=0.9; eps=1e-8;
+
+perfRecordAll = {}
+    
+for k in range(0, len(B2params)):
+    B2 = B2params[k]
+    W, b, y_pred, x, y, loss, training_op, reg = starter.buildGraph(lossType = 'MSE', B1 = B1, B2 = B2, eps = eps)
+    #Question 3, batch gradient descent
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:   
+        sess.run(init) #make a new graph. 
+        
+        n = len(trainTarget)
+        n_batches = int(n/batch_size)
+        perfRecord = starter.Performance(epochs)
+        
+        for i in range(epochs):
+            indices = np.random.permutation(n)
+            featVectors = trainData[indices]
+            classes = trainTarget[indices]    
+            
+            for j in range(0, n, batch_size):
+                            
+                #print(loss.eval())
+                sess.run(training_op, feed_dict = {x: featVectors, y: classes})
+                error = sess.run(loss, feed_dict = {x: featVectors, y: classes})
+                Weights = sess.run(W, feed_dict = {x: featVectors, y: classes})
+                Bias = sess.run(b, feed_dict = {x: featVectors, y: classes})                
+                _, accuracy, _ = starter.classify(Weights, Bias, trainData, trainTarget)
+                
+        
+            print('epoch #%i' % i)
+            #In the process of implementing saving the performance
+            #Save the per iteration errors: Error, Training Set performance, 
+            #Validation Set Performance, Test Set performance 
+            perfRecord.errorTrain[i] = sess.run(loss, feed_dict = {x: trainData, y: trainTarget})
+            perfRecord.errorValid[i] = sess.run(loss, feed_dict = {x: validData, y: validTarget})
+            perfRecord.errorTest[i] = sess.run(loss, feed_dict = {x: testData, y: testTarget})
+            _, perfRecord.trainSetAcc[i] ,_ = starter.classify(Weights, Bias, trainData, trainTarget)
+            _, perfRecord.validSetAcc[i] ,_ = starter.classify(Weights, Bias, validData, validTarget)
+            _, perfRecord.testSetAcc[i] ,_ = starter.classify(Weights, Bias, testData, testTarget)
+            
+        perfRecordAll["{0}".format(B2params[k])] = perfRecord #save the data     
+
+#%% Accuracy plots 
+
+plt.figure(figsize=(15,7))
+plt.subplot(1, 2, 1)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('TrainingSet Accuracy of Different Beta2 vs. Iterations')
+plt.plot(perfRecordAll["0.99"].trainSetAcc, 'r')
+plt.plot(perfRecordAll["0.9999"].trainSetAcc, 'b') 
+plt.legend(['Beta2 0.99, fin. acc = %f' % perfRecordAll["0.99"].trainSetAcc[-1] \
+            ,'Beta2 0.9999 fin. acc = %f' % perfRecordAll["0.9999"].trainSetAcc[-1]], loc = 4)
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy')
+plt.xlabel('Iterations')
+plt.title('ValidSet Accuracy of Different Beta2 vs. Iterations')
+plt.plot(perfRecordAll["0.99"].validSetAcc, 'r')
+plt.plot(perfRecordAll["0.9999"].validSetAcc, 'b') 
+plt.legend(['Beta2 0.99, fin. acc = %f' % perfRecordAll["0.99"].validSetAcc[-1] \
+            ,'Beta2 0.9999 fin. acc = %f' % perfRecordAll["0.9999"].validSetAcc[-1]], loc = 4)
+
+plt.savefig("Accuracy plot P3Q4b.png")
+                      
+                      
 #%% Q4a, MSE
 
 epochs = 700
@@ -101,7 +243,7 @@ plt.plot(perfRecordAll["0.99"].validSetAcc, 'b')
 plt.legend(['Beta1 0.95, fin. acc = %f' % perfRecordAll["0.95"].validSetAcc[-1] \
             ,'Beta1 0.99 fin. acc = %f' % perfRecordAll["0.99"].validSetAcc[-1]], loc = 4)
 
-plt.savefig("Accuracy plot P3Q4.png")
+plt.savefig("Accuracy plot P3Q4a.png")
 
 
 
@@ -117,7 +259,7 @@ batchSizeParams = [100, 700, 1750] #[500] #[100, 700, 1750]
 
 perfRecordAll = {}
 
-W, b, y_pred, x, y, loss, training_op, reg = starter.buildGraph(lossType = 'MSE')
+W, b, y_pred, x, y, loss, training_op, reg = starter.buildGraph(lossType = 'MSE', B1 = 0.9, B2 = 0.999, eps = 1e-08)
 
 #Question 3, batch gradient descent
 
@@ -236,10 +378,6 @@ plt.legend(['Batch Size 100, fin. acc = %f' % perfRecordAll["100"].testSetAcc[-1
             , 'Batch Size 1750 fin. acc = %f' % perfRecordAll["1750"].testSetAcc[-1]], loc = 4)
 
 plt.savefig("Accuracy plot P3Q3.png")
-
-#%% Question 4
-
-
 
 
 
