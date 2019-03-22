3450378eac9467163182ce46356df99b2b321dc6
SCPO <47129204+SCPO@users.noreply.github.com>
2019-03-22 15:25:23 -0400

    Add files via upload
    LossKMeans and distanceFucnt

--- starter_kmeans.py
+++ starter_kmeans.py
@@ -3,7 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
 
-# Loading data
+#Flags
+is_valid = False
+
+#% %# Loading data
+
 data = np.load('data2D.npy')
 #data = np.load('data100D.npy')
 [num_pts, dim] = np.shape(data)
@@ -17,6 +21,20 @@ if is_valid:
   val_data = data[rnd_idx[:valid_batch]]
   data = data[rnd_idx[valid_batch:]]
 
+#%% Global Parameters
+K = 3
+trainData = data
+
+
+#%% Tensorflow Model
+
+#Initialize the MU (centers), to be K random points. 
+[N, dim] = np.shape(data)
+randID = np.random.randint(N, size=K)
+centers = trainData[randID,:]
+
+
+
 
 # Distance function for K-means
 def distanceFunc(X, MU):
@@ -25,4 +43,33 @@ def distanceFunc(X, MU):
     # MU: is an KxD matrix (K means and D dimensions)
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
-    # TODO
+    
+    [dimX, _ ] = np.shape(X)
+    pair_dist = np.empty((dimX,0), dtype = 'float64')
+    
+    for center in MU: #Loop through each center
+        #Calculate distance to each datapoint
+        dist = np.linalg.norm(X - center, axis=1)
+        dist = dist.reshape((dimX,1))
+        #Concat it. 
+        pair_dist = np.hstack((pair_dist, dist))
+             
+    return pair_dist
+    
+    
+def lossKMeans(X, MU):
+    #Pairwise distance matrix calc
+    pairwiseDistMat = distanceFunc(X, MU) 
+    #Get the minimum of each row (i.e. the minimum center point distance, as per formula)
+    pairwiseDistMat = pairwiseDistMat.min(axis = 1)
+    #Square each dist
+    pairwiseDistMat = pairwiseDistMat**2
+    #Sum
+    loss = pairwiseDistMat.sum()
+    
+    return loss
+    
+    
+    
+    
+    
