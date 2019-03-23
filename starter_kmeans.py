fbd237c7ccf49c030316f14049771fa0744f8c17
SCPO <47129204+SCPO@users.noreply.github.com>
2019-03-23 00:16:14 -0400

    Part 1

--- starter_kmeans.py
+++ starter_kmeans.py
@@ -3,39 +3,56 @@ import numpy as np
 import matplotlib.pyplot as plt
 import helper as hlp
 
+#%%
 #Flags
 is_valid = False
 
 #% %# Loading data
 
 data = np.load('data2D.npy')
-#data = np.load('data100D.npy')
 [num_pts, dim] = np.shape(data)
 
-# For Validation set
-if is_valid:
-  valid_batch = int(num_pts / 3.0)
-  np.random.seed(45689)
-  rnd_idx = np.arange(num_pts)
-  np.random.shuffle(rnd_idx)
-  val_data = data[rnd_idx[:valid_batch]]
-  data = data[rnd_idx[valid_batch:]]
+#==============================================================================
+# # For Validation set
+# if is_valid:
+#   valid_batch = int(num_pts / 3.0)
+#   np.random.seed(45689)
+#   rnd_idx = np.arange(num_pts)
+#   np.random.shuffle(rnd_idx)
+#   val_data = data[rnd_idx[:valid_batch]]
+#   data = data[rnd_idx[valid_batch:]]
+#==============================================================================
 
 #%% Global Parameters
 K = 3
 trainData = data
+LEARNINGRATE = 0.001 #for ADAM
+epochs = 2000
 
 
 #%% Tensorflow Model
 
 #Initialize the MU (centers), to be K random points. 
-[N, dim] = np.shape(data)
+[N, dim] = np.shape(trainData)
 randID = np.random.randint(N, size=K)
-centers = trainData[randID,:]
+initCenters = trainData[randID,:] #CHANGE THIS TO SAPMLING FORM STANDARD NORMAL DIST
 
 
+X = tf.placeholder("float64", np.shape(trainData))
+centers = tf.Variable(initCenters)
+error = lossKMeans(X, centers)
+train_op = tf.train.AdamOptimizer().minimize(error)
 
 
+with tf.Session() as session:
+    init = tf.global_variables_initializer()
+    session.run(init)
+    for epo in range(epochs):
+        _, loss = session.run([train_op, error], {X: trainData})
+        print('loss = %f epochs = %i' % (loss, epo))
+
+        
+#%% Function Defs
 # Distance function for K-means
 def distanceFunc(X, MU):
     # Inputs
@@ -44,31 +61,40 @@ def distanceFunc(X, MU):
     # Outputs
     # pair_dist: is the pairwise distance matrix (NxK)
     
-    [dimX, _ ] = np.shape(X)
-    pair_dist = np.empty((dimX,0), dtype = 'float64')
-    
-    for center in MU: #Loop through each center
-        #Calculate distance to each datapoint
-        dist = np.linalg.norm(X - center, axis=1)
-        dist = dist.reshape((dimX,1))
-        #Concat it. 
-        pair_dist = np.hstack((pair_dist, dist))
+    #from https://databricks.com/tensorflow/clustering-and-k-means
+    expanded_vectors = tf.expand_dims(X, 0)
+    expanded_centroids = tf.expand_dims(MU, 1)
+    pair_dist = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroids)), 2)
              
     return pair_dist
     
     
 def lossKMeans(X, MU):
     #Pairwise distance matrix calc
-    pairwiseDistMat = distanceFunc(X, MU) 
-    #Get the minimum of each row (i.e. the minimum center point distance, as per formula)
-    pairwiseDistMat = pairwiseDistMat.min(axis = 1)
-    #Square each dist
-    pairwiseDistMat = pairwiseDistMat**2
-    #Sum
-    loss = pairwiseDistMat.sum()
     
-    return loss
+    expanded_vectors = tf.expand_dims(X, 0)
+    expanded_centroids = tf.expand_dims(MU, 1)
+    pair_dist = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroids)), 2)
     
+    mins = tf.argmin(pair_dist, 0)
+    mins = tf.math.square(mins)
+    mins = tf.math.reduce_sum(tf.cast(mins, "float64"))
+    return mins
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
